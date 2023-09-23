60eaf78aeed80d16d10fc858b7021b6f009dea27
noradroid <75635291+noradroid@users.noreply.github.com>
2023-09-24 01:05:33 +0800

    Add error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component, inject } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { ToastsService } from 'toasts';
 
+import {
+  BAD_GATEWAY,
+  BAD_REQUEST,
+  CONFLICT,
+  FORBIDDEN,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  SERVICE_UNAVAILABLE,
+  UNAUTHORIZED,
+} from './error-message.constants';
 import { ErrorType, ErrorTypes } from './error.type';
 import { ErrorsApiService } from './errors-api.service';
-import { HttpErrorResponse } from '@angular/common/http';
+import { StatusCode } from './status-code.enum';
 
 @Component({
   selector: 'app-root',
@@ -19,17 +30,50 @@ export class AppComponent {
   getError(errorType: ErrorType): void {
     this.service.getError(errorType).subscribe({
       next: (val) => {
-        console.log('val ' + JSON.stringify(val));
+        console.log(val);
       },
       error: (err: HttpErrorResponse) => {
         console.log(err);
-        if (err.status !== 0) {
-          this.toasts.info(JSON.stringify(err.error));
-        } else {
+        if (err.status === 0) {
+          this.toasts.info(
+            'An error has occurred. Pleas check the logs for more details.'
+          );
           console.log(err);
-          this.toasts.info(JSON.stringify(err.error));
+        } else {
+          this.serverErrorHandler(err);
         }
       },
     });
+  }
+
+  serverErrorHandler(err: HttpErrorResponse): void {
+    switch (err.status) {
+      case StatusCode.BAD_REQUEST:
+        this.toasts.info(BAD_REQUEST);
+        break;
+      case StatusCode.UNAUTHORIZED:
+        this.toasts.info(UNAUTHORIZED);
+        break;
+      case StatusCode.FORBIDDEN:
+        this.toasts.info(FORBIDDEN);
+        break;
+      case StatusCode.NOT_FOUND:
+        this.toasts.info(NOT_FOUND);
+        break;
+      case StatusCode.CONFLICT:
+        this.toasts.info(CONFLICT);
+        break;
+      case StatusCode.INTERNAL_SERVER_ERROR:
+        this.toasts.info(INTERNAL_SERVER_ERROR);
+        break;
+      case StatusCode.BAD_GATEWAY:
+        this.toasts.info(BAD_GATEWAY);
+        break;
+      case StatusCode.SERVICE_UNAVAILABLE:
+        this.toasts.info(SERVICE_UNAVAILABLE);
+        break;
+      default:
+        this.toasts.info(JSON.stringify(err.error));
+    }
   }
 }
---
+++ src/app/error-message.constants.ts
@@ -0,0 +1,20 @@
+export const BAD_REQUEST =
+  'Bad request. Please provide valid inputs and try again.';
+
+export const UNAUTHORIZED = 'Unauthorized. Please login and then try again.';
+
+export const FORBIDDEN =
+  'Forbidden. You do not have the permissions to access this resource.';
+
+export const NOT_FOUND = 'Resource not found.';
+
+export const CONFLICT = 'Conflict. This resource already exists.';
+
+export const INTERNAL_SERVER_ERROR =
+  'Internal server error. Please try again later.';
+
+export const BAD_GATEWAY =
+  'Bad gateway. Please check your gateway configurations.';
+
+export const SERVICE_UNAVAILABLE =
+  'Service unavailable. Please check if the service is available.';

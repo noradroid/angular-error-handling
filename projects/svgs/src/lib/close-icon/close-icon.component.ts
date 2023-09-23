9b7f95243368989ed9bc8259ca100b6b098e1f69
noradroid <75635291+noradroid@users.noreply.github.com>
2023-09-24 00:42:27 +0800

    Update svg library close icon and update toast library toast styling

--- projects/svgs/src/lib/close-icon/close-icon.component.ts
+++ projects/svgs/src/lib/close-icon/close-icon.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'icon-close',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './close-icon.component.html',
   styleUrls: ['./close-icon.component.scss'],
 })
-export class CloseIconComponent {}
+export class CloseIconComponent {
+  faXmark = faXmark;
+}
